676f4938da8ee59098e1389305e5e10d290be24d
Arjunbala <arjunbala0711@gmail.com>
2013-09-16 12:10:27 +0530

    Insert Operation Complete

--- threaded trees.cpp
+++ threaded trees.cpp
@@ -1,12 +1,21 @@
 // in-threaded trees
+using namespace std;
+
 
 #include<iostream>
 #include<conio.h>
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<iomanip.h>
+#include<deque>
+#include<sstream>
+#include<math.h>
 
-enum boolean
+enum status
 {
-     not_threaded = 0;
-     threaded = 1;
+      structural = 0,threaded
 };
 
 struct node 
@@ -14,11 +23,11 @@ struct node
        int data;
        node *left;
        node *right;
-       enum boolean isleft;
-       enum boolean isright;
+       enum status isleft;
+       enum status isright;
 };
 
-node *head = NULL;
+node *phead = NULL;
 
 void insert(node *head)
 {
@@ -31,16 +40,94 @@ void insert(node *head)
      ins->isright = threaded;
      if(head == NULL) // tree empty in this case
      {
-         head->data = -9999;
-             
+         node *s = new node;    
+         s->data = 0;
+         s->left = ins;
+         s->right = head;
+         s->isleft = structural;
+         s->isright = structural;
+         ins->left = s;
+         ins->right = s;
+         phead = s;
+         return;
+     }
+     else
+     {
+         node *p = head->left;
+         while(p != head)
+         {
+          if(no < p->data)
+          {
+               if(p->isleft == structural)
+               {
+                            p = p->left;
+               }
+               else if(p->isleft == threaded)
+               {
+                    ins->left = p->left;
+                    p->left = ins;
+                    p->isleft = structural;
+                    ins->right = p;
+                    return;
+               }
+          }
+          else if(no > p->data)
+          {
+               if(p->isright == structural)
+               {
+                  p = p->right;
+               }
+               else if (p->isright == threaded)
+               {
+                    ins->right = p->right;
+                    p->right = ins;
+                    p->isright = structural;
+                    ins->left = p;
+                    return;
+               }
+          }
+         }
      }
 }
 
-void main()
+
+
+
+void tree_disp(node *p)
+{
+
+     if(p->isleft == structural)
+     {
+                  tree_disp(p->left);
+     }
+     cout<<p->data<<" ";
+     if(p->isright == structural)
+     {
+                   tree_disp(p->right);
+     } 
+}
+
+void display(node *head)
+{
+     if(head == NULL)
+     {
+             cout<<"Tree is empty\n";
+             return;
+     }
+     else
+     {
+         cout<<"\n\nCurrent tree: ";
+         node *p = head->left;
+         tree_disp(p);
+     }
+}
+
+int main()
 {
      int ch =1;
      while(ch != 3)
      {
+              cout<<"\n\n";
               cout<<"1.Insert\n";
               cout<<"2.Display\n";
               cout<<"3.Exit\n";
@@ -48,14 +135,15 @@ void main()
               cin>>ch;
               switch(ch)
               {
-                        case 1: insert(head);
+                        case 1: insert(phead);
                                 break;
-                        case 2: display(head);
+                        case 2: display(phead);
                                 break;
                         case 3: break;
                         default: cout<<"Enter a valid choice \n";
               }
      }
      getch();
+     return 0;
 }
 
